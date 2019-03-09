e686e788f875fccfe09a25adab08dedea1d2b8a1
Kirill Belonogov <kirill.belonogov@gmail.com>
2019-03-09 12:00:00 +0300

    Wip

---
+++ Assets/NewScripts/DynamicScrollList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DynamicScrollList : MonoBehaviour, IDragHandler
+{
+    public event Action<float> onScroll;
+
+    [SerializeField] 
+    RectTransform.Axis _axis;
+    [SerializeField] 
+    RectTransform _viewport;
+    [SerializeField]
+    RectTransform _content;
+
+    Canvas _canvas;
+
+    void Awake()
+    {
+        _canvas = _viewport.GetComponentInParent<Canvas>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 localDelta = RectTransformHelpers.ScreenPointToLocalPoint(eventData.delta, _canvas);
+        OnScroll(_axis == RectTransform.Axis.Horizontal ? localDelta.x : localDelta.y);
+    }
+
+    void OnScroll(float delta)
+    {
+        Vector2 mask = _axis == RectTransform.Axis.Horizontal ? Vector2.right : Vector2.up;
+        _content.anchoredPosition += mask * delta;
+        
+        onScroll?.Invoke(delta);
+    }
+}
+
+static class RectTransformHelpers
+{
+    public static Vector2 ScreenPointToLocalPoint(Vector2 screenPoint, Canvas canvas)
+    {
+        return screenPoint / canvas.scaleFactor;
+    }
+}
