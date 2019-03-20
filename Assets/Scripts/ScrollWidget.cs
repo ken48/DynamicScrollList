71751b6c0009fdf07e3fb7fefe443bbb753221d1
 Kirill Belonogov <k_belonogov@wargaming.net>
2019-03-20 10:25:26 +0300

    Wip

--- Assets/Scripts/ScrollWidget.cs
+++ Assets/Scripts/ScrollWidget.cs
@@ -15,7 +15,9 @@ public class ScrollWidget : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
     Vector2 _finishPosition;
     Vector2 _lastDelta;
     bool _isDragging;
-    Vector2 _velocity;
+    Vector2 _inertiaVelocity;
+    Vector2 _edgesDelta;
+    float _elasticity;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -23,8 +25,9 @@ public class ScrollWidget : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
             eventData.pressEventCamera, out _startPosition))
         {
             _isDragging = true;
-            _velocity = Vector2.zero;
-            // _elasticityCoef = 1f;
+            _inertiaVelocity = Vector2.zero;
+            _edgesDelta = Vector2.zero;
+            _elasticity = 1f;
         }
     }
 
@@ -33,56 +36,76 @@ public class ScrollWidget : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
         if (!_isDragging)
             return;
 
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(_viewport, eventData.position,
-            eventData.pressEventCamera, out Vector2 finishPosition);
-
-        Vector2 mask = _axis == RectTransform.Axis.Horizontal ? Vector2.right : Vector2.up;
-        Vector2 delta = Vector2.Scale(finishPosition - _startPosition, mask);
-
-        // if (_elasticityCoef < 1f)
-        //     localDelta *= _elasticityCoef * 0.01f;
+        Vector2 delta = GetDeltaPosition(eventData);
+        _lastDelta = delta;
 
         OnScroll(delta);
-
-        _lastDelta = delta;
-        _startPosition = finishPosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        if (_velocity.sqrMagnitude <= 0f)
-            _velocity = _lastDelta;
+        if (!_isDragging)
+            return;
+
+        Vector2 delta = GetDeltaPosition(eventData);
+        _inertiaVelocity = _lastDelta + delta;
         _isDragging = false;
+
+        OnScroll(delta);
     }
 
     public void SetEdgesDelta(float edgesDelta)
     {
-        // if (_isDragging)
-            // _elasticityCoef = 1f - Mathf.Clamp01(Mathf.Abs(edgesDelta) / _viewport.rect.height);
+        if (_isDragging)
+            _elasticity = 1f - Mathf.Clamp01(Mathf.Abs(edgesDelta) / _viewport.rect.height);
 
         Vector2 mask = _axis == RectTransform.Axis.Horizontal ? Vector2.right : Vector2.up;
-        _velocity = mask * edgesDelta;
+        _edgesDelta = mask * edgesDelta;
     }
 
     void LateUpdate()
     {
-        if (_isDragging || _velocity.sqrMagnitude <= 0f)
+        // Todo: strange bug with inifinite scrolling (maybe _edgesDelta stays const)
+
+        if (_isDragging || (!CheckVectorMagnitude(_inertiaVelocity) && !CheckVectorMagnitude(_edgesDelta)))
             return;
 
         // Todo: max speed restriction
-        const float speedCoef = 25f;
-        const float inertiaCoef = 5.5f;
+        const float speedCoef = 13f;
+        const float inertiaCoef = 5f;
 
         float dt = Time.unscaledDeltaTime;
-        Vector2 delta = _velocity * speedCoef * dt;
-        _velocity *= 1f - Mathf.Clamp01(dt * inertiaCoef);
+        Vector2 totalVelocity = _inertiaVelocity + _edgesDelta;
+        Vector2 delta = totalVelocity * speedCoef * dt;
+        _inertiaVelocity *= 1f - Mathf.Clamp01(dt * inertiaCoef);
 
         OnScroll(delta);
     }
 
     void OnScroll(Vector2 delta)
     {
+        if (!CheckVectorMagnitude(delta))
+            return;
+
         onScroll?.Invoke(_axis == RectTransform.Axis.Horizontal ? delta.x : delta.y);
         _finishPosition += delta;
+    }
+
+    Vector2 GetDeltaPosition(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_viewport, eventData.position, eventData.pressEventCamera,
+            out Vector2 finishPosition);
+        Vector2 mask = _axis == RectTransform.Axis.Horizontal ? Vector2.right : Vector2.up;
+        Vector2 delta = Vector2.Scale(finishPosition - _startPosition, mask);
+        _startPosition = finishPosition;
+
+        if (_elasticity < 1f)
+            delta *= _elasticity * 0.5f;
+        return delta;
+    }
+
+    static bool CheckVectorMagnitude(Vector2 v)
+    {
+        return v.sqrMagnitude >= 0.0001f;
     }
 }
